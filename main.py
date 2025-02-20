c5efd2c8d6292beb7da97e14c64e9f84978e3454
guimeb007 <39763499-guimeb007@users.noreply.replit.com>
2025-02-20 19:41:10 +0000

    User checkpoint: Fix: Correct config file path and move config.json to configs/ directory

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from logs.log import logger
 
 # 🟢 JSON Configuration
 if not os.path.isfile(
-        f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+        f"{os.path.realpath(os.path.dirname(__file__))}/configs/config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
 else:
     with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"
